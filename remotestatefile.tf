8e760fc14d96d850ef48adb7ecc0b253c77344ba
lina7500 <50518310+lina7500@users.noreply.github.com>
2023-11-13 16:38:26 +0100

    Update remotestatefile.tf

--- remotestatefile.tf
+++ remotestatefile.tf
@@ -1,1 +1,23 @@
+terraform {
+  backend "s3" {
+    bucket = "lintfstatebucket"
+    key = "global/s3/dev/terraform.tfstate"
+    region = "${var.region_name}"
+    encrypt = true
+  }
+}
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "lintfstatebucket"
+  lifecycle {
+    prevent_destroy = true
+  }
+  /*
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }*/
+}
 
